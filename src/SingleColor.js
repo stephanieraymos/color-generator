c198e7b0d523b2abad2898cee442cd5d36e31ba0
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 11:17:15 -0800

    Changing font color based on tint/shade index value

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
-    console.log(hexColor);
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(","); //All values joined + seperated by commas
   const hex = rgbToHex(...rgb);
 
   return (
-    <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">{hex}</p>
+      <p className="color-value">{hexColor}</p>
     </article>
   );
 };
