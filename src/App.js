0b4df0abc6e234960195bace5a50398dc958977c
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 10:04:11 -0800

    Base plate

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+import Values from "values.js";
+
+function App() {
+  return <h2>Color generator project</h2>;
+}
+
+export default App;
