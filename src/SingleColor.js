86eef024bbddc9d8d95793ebfaf6abf32c36c7b7
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 11:06:36 -0800

    Weight (%) is displaying over color

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
 const SingleColor = ({ rgb, weight, index }) => {
-    const [alert, setAlert] = useState(false);
-    const bcg = rgb.join(',') //All values joined + seperated by commas
-    console.log(bcg);
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(","); //All values joined + seperated by commas
+  console.log(bcg);
   return (
-  <article className={`color`} style={{ backgroundColor:`rgb(${bcg})`}}>
-      { ' ' }
-  </article>
+    <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
+      <p className="percent-value">{weight}%</p>
+    </article>
   );
 };
 
