80e40a4679b97e26b76be4975d565224bc3a6f1b
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 13:32:29 -0800

    Added hashtag to values

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,14 +5,14 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(","); //All values joined + seperated by commas
   const hex = rgbToHex(...rgb);
-
+    const hexValue = `#${hexColor}`
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
     >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">{hexColor}</p>
+      <p className="color-value">{hexValue}</p>
     </article>
   );
 };
