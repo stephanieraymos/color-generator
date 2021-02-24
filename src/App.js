4be64c20b0ea61ba5c5fed8012a9cce466898f7f
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 10:15:47 -0800

    Form and colors sections

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
 
-    const [color, setColor] = useState('');
-    const [error, setError] = useState(false);
-    const [list, setList] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Testing");
+  };
 
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Testing')
-    }
-    
-  return <>
-  
-  </>;
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSumbit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+        </form>
+        J
+      </section>
+      <section className="colors">
+        <h4>List of colors goes here.</h4>
+      </section>
+    </>
+  );
 }
 
 export default App;
