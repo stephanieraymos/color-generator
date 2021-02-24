9c2244d151a8f67f1d162fd9d4d14d54dd05c6cb
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 13:43:33 -0800

    Alert disappears + timeout clears after 3 seconds

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -6,6 +6,14 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const bcg = rgb.join(","); //All values joined + seperated by commas
   const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+        setAlert(false)
+    }, 3000) //3 seconsd
+    return () => clearTimeout(timeout)
+  }, [alert]) //Once the alert changes
+  
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
