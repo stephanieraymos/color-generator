9e6c1f0ea02c12242f7430136b5c6e412d68656c
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 13:38:46 -0800

    Copying to clipboard successfully

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,14 +5,19 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(","); //All values joined + seperated by commas
   const hex = rgbToHex(...rgb);
-    const hexValue = `#${hexColor}`
+  const hexValue = `#${hexColor}`;
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+          setAlert(true);
+          navigator.clipboard.writeText(hexValue)
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">Copied to clipboard</p>}
     </article>
   );
 };
